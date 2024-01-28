0031f3467b0218e2458b1956153580807aa7c6ba
hitfarman <hitfarman@gmail.com>
2024-01-28 10:09:13 -0800

    modify component fetch multi home page data solution 1

--- src/store/modules/home.js
+++ src/store/modules/home.js
@@ -8,17 +8,18 @@ export const fetchHomeDataAction = createAsyncThunk("fetchdata", async (payload,
 
   // 但是, 也可以, 在service中的module中建一个单独的模块, 把每个请求的url, 请求参数等写好, 做进一步的封装;
   // 在这里就可以直接调这个封装的模块请求接口
-  const res = await getHomeGoodPriceData()
-  // dispatch(changeGoodPriceInfoAction(res))
+  const res1 = await getHomeGoodPriceData()
+  const res2 = await getHomeGoodPriceData()
 
-  return res
+  return {goodPrice: res1, highScore: res2}
 })
 
 const homeSlice = createSlice({
   name: "home",
   initialState: {
     // 应该保存一个对象. 它不仅仅是个列表,它还包含title等数据,标题title也是属于数据的一部分的;有的还有子标题
-    goodPriceInfo: {}
+    goodPriceInfo: {},
+    highScoreInfo: {}
   },
   reducers: {
     changeGoodPriceInfoAction(state, { payload }) {
@@ -27,7 +28,8 @@ const homeSlice = createSlice({
   },
   extraReducers: (builder) => {
     builder.addCase(fetchHomeDataAction.fulfilled, (state, { payload }) => {
-      state.goodPriceInfo = payload
+      state.goodPriceInfo = payload.goodPrice
+      state.highScoreInfo = payload.highScore
     })
   }
 })
--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -8,14 +8,12 @@ import SectionHeader from '@/components/section-header'
 import SectionRoomList from '@/components/section-room-list'
 
 const Home = memo(() => {
-  /** 从redux中获取数据: useSelector返回一个state, 直接结构,就可以拿到state中的数据了 */
   const { goodPriceInfo } = useSelector((state) => ({
     goodPriceInfo: state.home.goodPriceInfo
   }), shallowEqual)
 
   /** 派发异步事件: 发送网络请求 */
   const dispatch = useDispatch()
-  // 发起网络请求, 但是,真正发起网络请求的请求逻辑不放到业务组件里,而是放在redux的store中
   useEffect(() => {
     dispatch(fetchHomeDataAction())
   }, [dispatch])
